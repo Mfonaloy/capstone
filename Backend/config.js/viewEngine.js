fd5d28840c4cc2b83c2c46365a2da44c8155445e
Mfon Aloy <mfonobongsmn82@gmail.com>
2024-04-02 16:27:41 +0100

    Initial commit

---
+++ Backend/config.js/viewEngine.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+
+let configViewEngine = (app) => {
+
+}
+
+module.exports = configViewEngine();
---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+require("dotenv").config();
+import express from "express";
+
+import configViewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web";
+import connectDB from "./config/connectDB";
+
+let app = express();
+
+//config view Engine
+configViewEngine(app);
+
+
+connectDB();
+//init all web routes
+initWebRoutes(app);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+   console.log(`App is running at the port ${port}`);
+});
